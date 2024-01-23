8b1ea05166244f6368a65e2e8bad76de10d3d786
Jason897571 <king.gs1314@outlook.com>
2024-01-23 12:32:43 -0500

    complete questions

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of your readme?",
+    name: "title"
+},{
+    type: "input",
+    message: "Please enter your project description:",
+    name: "description"
+},{
+    type: "input",
+    message: "Please enter installation instructions for your project:",
+    name: "installation_instructions"
+},{
+    type: "input",
+    message: "Please enter usage information for your project:",
+    name: "usage_information"
+},{
+    type: "input",
+    message: "Please enter contribution guidelines for your project:",
+    name: "contribution_guidelines"
+},{
+    type: "input",
+    message: "Please enter test instructions for your project:",
+    name: "test_instructions"
+},{
+    type: "list",
+    message: "Please choose your license for the project:" ,
+    name: "license",
+    choices:["MIT License","Apache License 2.0","Boost Software License 1.0"]
+},{
+    type: "input",
+    message: "Please enter your github username:",
+    name: "github_username"
+},{
+    type: "input",
+    message: "Please enter your email address:",
+    name: "email_address"
+}];
+inquirer
+    .prompt(questions)
+    .then(
+        data =>{
+            console.log(data)
+           
+        }
+    )
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
